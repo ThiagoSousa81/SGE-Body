9a07ad07c6d98cd083137a125419ae8646e13c5b
Thiago Sousa <thiagosousa81@gmail.com>
2023-09-12 00:09:27 -0300

    §

---
+++ htdocs/js/footer.php
@@ -0,0 +1,24 @@
+<?php
+echo '
+function Footer() {
+    return (
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div class="container-fluid">
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                <p class="navbar-text">2023 © - Desenvolvido por <a href="https://ebs-systems.epizy.com/">Thiago Sousa - EBS Security Systems</a></p>
+            </li>            
+            </ul>
+        </div>
+        </nav>
+    );
+}'/*<li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>*/
+?>
