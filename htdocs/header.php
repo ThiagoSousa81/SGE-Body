4bf855deb7878769eeeecf0c27da0ddc722a55c3
Thiago Sousa <thiagosousa81@gmail.com>
2023-12-24 23:14:23 -0300

    §

--- htdocs/header.php
+++ htdocs/header.php
@@ -20,4 +20,5 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
     <link href="/src/styles.css" rel="stylesheet">
+    <script src="/src/script.js" defer></script>
 </head>
