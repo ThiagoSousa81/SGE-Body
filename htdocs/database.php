c192742d28f1225b6e8b66ece8fb7134f9765fc6
Thiago Sousa <thiagosousa81@gmail.com>
2023-12-26 23:22:38 -0300

    §

--- htdocs/database.php
+++ htdocs/database.php
@@ -1,45 +1,55 @@
 <?php
-Class database 
+Class database  
 {
-    protected $usuario = 'xxxx';
-    protected $senha = 'xxxx';
-    protected $database = 'xxxxxx';
-    protected $host = 'xxxxx';
+    private $usuario = 'xxx';
+    private $senha = 'xxxx';
+    private $database = 'xxxx';
+    private $host = 'xxxxx';
     
     public function GetLinkMySQLI()
     {
-        $mysqli = new mysqli($host, $usuario, $senha, $database);
+        $mysqli = new mysqli($this->host, $this->usuario, $this->senha, $this->database);
         return $mysqli;
     }
     
-    public function login($user, $password)
-    { // O PDO prepare é utilizado para evitar injeção de código
-        global $pdo;  
-        
+    
+
+    public function login($user, $password){
         try {
-            $pdo = new PDO("mysql:dbname=".$database.";host=".$host,$usuario,$senha);
+            $pdo = new PDO("mysql:dbname=" . $this->database . ";host=" . $this->host, $this->usuario, $this->senha);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Erro na conexão com o banco de dados: " . $e->getMessage());
         }
-        catch (PDOException $e) {
-            $msg = $e->getMessage();
-        }
-        
-        $sql = $pdo->prepare("SELECT ID_USER FROM USER WHERE EMAIL_USER = :u AND HASH_USER = :h");  
+
+        $sql = $pdo->prepare("SELECT ID_USER, EMAIL_USER, HASH_USER FROM USER WHERE EMAIL_USER = :u");
         $sql->bindValue(":u", $user);
-        $sql->bindValue(":h", $password);  
-        //$sql = $pdo->prepare("SELECT id_user FROM usuarios WHERE nome_user = '".$user."' AND hash_user = '".$password."'");  
         $sql->execute();
-        if($sql->rowCount() > 0)
-        {
-            $dados = $sql->fetch();
-            session_set_cookie_params(1800); //Define o tempo limite da sessão 30 min
+
+        $result = $sql->fetch(PDO::FETCH_ASSOC);
+
+        if ($result && ($password == $result['HASH_USER'])) {
+            session_set_cookie_params(1800);
             session_start();
-            $_SESSION['ID_USER'] = $dados['ID_USER'];                
+            $_SESSION['ID_USER'] = $result['ID_USER'];
             return true;
         }
-        else
-        {
+        else {
             return false;
-        }
+        }            
+    }
+
+    public function logout()
+    {
+        session_start();
+        session_unset();
+        session_destroy();
+    }
+
+    public function isLoggedIn()
+    {
+        session_start();
+        return isset($_SESSION['ID_USER']);
     }
 
     
