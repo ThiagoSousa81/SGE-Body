20c7f8d11264a925330c5b3e3efc3e2ac4a2063a
Thiago Sousa <thiagosousa81@gmail.com>
2024-01-12 08:48:52 -0300

    Update dashboards.php

--- htdocs/dashboards.php
+++ htdocs/dashboards.php
@@ -3,8 +3,85 @@ require "protection.php";
 include "index.php";
 ?>
 
-<h3>Dashboards</h3>
-<p>Em breve...</p>
+<div class="cont">
+
+<div class="text-dark">
+  <div class="rounded bg-light p-3" style="
+    margin-block-end: 20px;
+">
+    <h4>Dashboard</h4>
+    <p>Algum texto..</p>
+  </div>
+  <div class="row" style="
+    margin-block-end: 20px;
+">
+    <div class="col-sm-3">
+      <div class="rounded bg-light p-3">
+        <h4>Usuários</h4>
+        <p>1 Milhão</p> 
+      </div>
+    </div>
+    <div class="col-sm-3">
+      <div class="rounded bg-light p-3">
+        <h4>Páginas</h4>
+        <p>100 Milhões</p> 
+      </div>
+    </div>
+    <div class="col-sm-3">
+      <div class="rounded bg-light p-3">
+        <h4>Sessões</h4>
+        <p>10 Milhões</p> 
+      </div>
+    </div>
+    <div class="col-sm-3">
+      <div class="rounded bg-light p-3">
+        <h4>Taxa de Rejeição</h4>
+        <p>30%</p> 
+      </div>
+    </div>
+  </div>
+  <div class="row" style="
+    margin-block-end: 20px;
+">
+    <div class="col-sm-4">
+      <div class="rounded bg-light p-3">
+        <p>Texto</p> 
+        <p>Texto</p> 
+        <p>Texto</p> 
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="rounded bg-light p-3">
+        <p>Texto</p> 
+        <p>Texto</p> 
+        <p>Texto</p> 
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="rounded bg-light p-3">
+        <p>Texto</p> 
+        <p>Texto</p> 
+        <p>Texto</p> 
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-8">
+      <div class="rounded bg-light p-3">
+        <p>Texto</p> 
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="rounded bg-light p-3">
+        <p>Texto</p> 
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+</div>
 
 
 </div>
